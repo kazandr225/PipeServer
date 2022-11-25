a87babc69c3a90e281ab45ba73a45ca29a9263a4
Данил Казанский <danii.kazanskiy@mail.ru>
2022-11-25 16:26:29 +0300

    Закончил

--- PipeServer/Source.c
+++ PipeServer/Source.c
@@ -1,40 +1,58 @@
 //код сервера
 
+#define SIZE_BUFFER 140
+
 #include <stdio.h>
 #include <Windows.h>
 
 int main()
 {
 	system("chcp 1251<nul");
+	
+	HANDLE hNamedPipe;
+	LPSTR lpszPipeName = L"\\\\.\\pipe\\MyPipe";
+	DWORD size_buffer = SIZE_BUFFER;
+	LPWSTR buffer = (CHAR*)calloc(size_buffer, sizeof(CHAR));
+	char message[SIZE_BUFFER];
+	BOOL Connected;
+	DWORD actual_readen;
+	BOOL SuccessRead;
+
 	while (TRUE)
 	{
-		HANDLE hNamedPipe;
-		LPSTR lpszPipeName = L"\\\\.\\pipe\\MyPipe";
+		
+		
 		hNamedPipe = CreateNamedPipe(
 			lpszPipeName,
 			PIPE_ACCESS_DUPLEX,
 			PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
 			PIPE_UNLIMITED_INSTANCES,
-			512,
-			512,
+			SIZE_BUFFER,
+			SIZE_BUFFER,
 			INFINITE,
 			NULL);
-
-		BOOL Connected = ConnectNamedPipe(hNamedPipe, NULL);
+		Connected = ConnectNamedPipe(hNamedPipe, NULL); //Установка соединения клиента с каналом
 		if (Connected)
 		{
-			printf("\nКлиент успешно подключился\n");
-			DWORD size_buffer = 100;
-			LPWSTR buffer = (CHAR*)calloc(size_buffer, sizeof(CHAR));
-
-			DWORD actual_readen;
-			BOOL SuccessRead = ReadFile(hNamedPipe, buffer, size_buffer, &actual_readen, NULL);
-			if (!SuccessRead)
+			//printf("\nКлиент успешно подключился\n");
+				
+			SuccessRead = ReadFile(hNamedPipe, buffer, size_buffer, &actual_readen, NULL);
+			if (SuccessRead)
+			{
+				printf("\nКлиент пишет: \n");
+				printf(buffer);
+				printf("\n");
+				//Отвечаем клиенту
+				printf("\nВведите сообщение для клиента: \n");
+				gets(message);
+				buffer = &message;
+				WriteFile(hNamedPipe, buffer, size_buffer, &actual_readen, NULL);
+			}
+			else
 			{
 				printf("\nКлиент отключился от сервера\n");
 			}
-			printf(buffer);
-			CloseHandle(hNamedPipe);
+			CloseHandle(hNamedPipe); //закрываем канал
 		}
 	}
 }
