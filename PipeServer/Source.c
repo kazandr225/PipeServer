0813eb2477f5cd6e94e9a7867f1ec39becb67941
Данил Казанский <danii.kazanskiy@mail.ru>
2022-11-28 14:51:39 +0300

    asd

--- PipeServer/Source.c
+++ PipeServer/Source.c
@@ -1,26 +1,27 @@
 //код сервера
-
+#define _CRT_SECURE_NO_WARNINGS
 #define SIZE_BUFFER 140
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <Windows.h>
-char* itoa(int value, char* string, int radix);
-
-int vozv(int num, int res)
-{
-	res = num * num;
-}
+char* itoa(int value, char* buffer, float radix);
+double atof(const char* str);
 
 int main()
 {
 	system("chcp 1251>nul");
 	
+	//запускаем консоль в фоновом режиме
+	HWND hWnd = GetConsoleWindow();
+	ShowWindow(hWnd, SW_HIDE);
+
 	HANDLE hNamedPipe;
 	LPSTR lpszPipeName = L"\\\\.\\pipe\\MyPipe";
 	DWORD size_buffer = SIZE_BUFFER;
 	LPWSTR buffer = (CHAR*)calloc(size_buffer, sizeof(CHAR));
 	char res[SIZE_BUFFER];
+	char message[SIZE_BUFFER];
 	//char message[SIZE_BUFFER];
 	BOOL Connected;
 	DWORD actual_readen;
@@ -45,38 +46,49 @@ int main()
 			//printf("\nКлиент успешно подключился\n");
 				
 			SuccessRead = ReadFile(hNamedPipe, buffer, size_buffer, &actual_readen, NULL);
-			if (SuccessRead)
-			{
 				printf("\nКлиент пишет: \n");
 				printf(buffer);
 				printf("\n");
 				
 				//Переводим в число и считаем
-				char* Str = buffer;
-				int Num = 0;
-				Num = atoi(Str);
-				int res = Num * Num;
+				
+				//char* Str = buffer;
+				
+				/*Num = atoi(Str);
+				
 				char string[25];
-				itoa(res, string,10);
+				itoa(res, string,10);*/
 
-				char g;
+				/*char g;
 				char c[50];
-				g = atof(res);
+				g = atof(res);*/
+				float Num = atof(buffer);
 
+				if (Num = atof(buffer))
+				{
+					sprintf(message, "%g", Num * Num);
+					//_itoa(Num, buffer, 10);
+					buffer = &message;
+					WriteFile(hNamedPipe, message, size_buffer, &actual_readen, NULL);
+				}
+				else
+				{
+					WriteFile(hNamedPipe, "Некорректное значение", size_buffer, &actual_readen, NULL);
+				}
 				//Отвечаем клиенту
-				printf("\nВведите сообщение для клиента: \n");
+				/*printf("\nВведите сообщение для клиента: \n");*/
 				
 				/*gets(message);
 				buffer = &message;*/
 
-				gets(res);
-				buffer = &res;
-				WriteFile(hNamedPipe, buffer, size_buffer, &actual_readen, NULL);
-			}
-			else
-			{
+				/*gets(res);
+				buffer = &res;*/
+				
+				//WriteFile(hNamedPipe, buffer, size_buffer, &actual_readen, NULL);
+				if (!SuccessRead)
+				{
 				printf("\nКлиент отключился от сервера\n");
-			}
+				}
 			CloseHandle(hNamedPipe); //закрываем канал
 		}
 	}
